01dcbef0e09f05cc3384b91c36f75d392f9d23e4
Joshua Bird <joshua.thomas.bird@gmail.com>
2016-12-05 14:18:02 -0500

    add chard to relevence histogram

--- client/app/routes/dashboards/dashboard.js
+++ client/app/routes/dashboards/dashboard.js
@@ -8,21 +8,31 @@ export default Ember.Route.extend({
     model: function(params) {
         return {
             overview: {
+                dasboardName: 'Institution Overview Dashboard',
                 widgets: [
+                    {chartType: 'totalResults', widgetType: 'number-widget', name: 'Total Results'},
+                    {chartType: 'totalPublications', widgetType: 'number-widget', name: 'Total Publications'},
                     {chartType: 'timeseries', widgetType: 'generic-chart', name:'timeser'},
                     {chartType: 'donut', widgetType: 'generic-chart', name: 'don1'},
                     {chartType: 'donut', widgetType: 'generic-chart', name: 'don2'}
                 ]
             },
             user: {
+                dasboardName: 'Institution Contributor Overview Dashboard',
                 widgets: [
-                    {chartType: 'timeseries', widgetType: 'generic-chart', name:'timeser'},
-                    {chartType: 'donut', widgetType: 'generic-chart', name: 'don1'},
-                    {chartType: 'donut', widgetType: 'generic-chart', name: 'don2'}
+                    {chartType: 'relevanceHistogram', widgetType: 'generic-chart', name:'timeser'}
+                    //{chartType: 'totalResults', widgetType: 'number-widget', name: 'Total Results'},
+                    //{chartType: 'totalPublications', widgetType: 'number-widget', name: 'Total Publications'},
+                    //{chartType: 'timeseries', widgetType: 'generic-chart', name:'timeser'},
+                    //{chartType: 'donut', widgetType: 'generic-chart', name: 'don1'},
+                    //{chartType: 'donut', widgetType: 'generic-chart', name: 'don2'}
                 ]
             },
             topic: {
+                dasboardName: 'Institution Subject Area Dashboard',
                 widgets: [
+                    {chartType: 'totalResults', widgetType: 'number-widget', name: 'Total Results'},
+                    {chartType: 'totalPublications', widgetType: 'number-widget', name: 'Total Publications'},
                     {chartType: 'timeseries', widgetType: 'generic-chart', name:'timeser'},
                     {chartType: 'donut', widgetType: 'generic-chart', name: 'don1'},
                     {chartType: 'donut', widgetType: 'generic-chart', name: 'don2'}

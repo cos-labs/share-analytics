3c2301237e14a27b4df306238b6cf5b6939978a0
Joshua Bird <joshua.thomas.bird@gmail.com>
2017-01-19 12:51:26 -0500

    better ucsd styling

--- client/app/components/number-widget/component.js
+++ client/app/components/number-widget/component.js
@@ -1,20 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  total: 'hello',
-  settings : {
-      fontSize: 3,
-      fontColor: '#F44336',
-      pre:'',
-      post: '',
-      value: 0
-  },
-  init () {
-      this._super(...arguments);
-      let settings = this.get('widgetSettings');
-      if(settings){
-          this.set('settings', settings);
-      }
-  }
+
+    total: 'hello',
+
+    settings : {
+        fontSize: 3,
+        fontColor: '#F44336',
+        pre:'',
+        post: '',
+        value: 0
+    },
+
+    init () {
+        this._super(...arguments);
+        let settings = this.get('widgetSettings');
+        if(settings){
+            this.set('settings', settings);
+        }
+    },
+
+    didReceiveAttrs() {
+        this._super(...arguments);
+        var unformatted = this.total
+        if (!unformatted) {
+            unformatted = this.settings.value
+        }
+        this.set('total', unformatted.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    }
 
 });

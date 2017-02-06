d057ed397fa9a5354b559c92700bcb8dd0285eca
Josh Bird <joshua.thomas.bird@gmail.com>
2017-02-06 11:05:39 -0500

    fix height on stafcked bars widget

--- client/config/environment.js
+++ client/config/environment.js
@@ -45,11 +45,11 @@ module.exports = function(environment) {
         ENV['ember-cli-mirage'] = {
             enabled: false
         };
-        ENV.APP.LOG_RESOLVER = true;
-        ENV.APP.LOG_ACTIVE_GENERATION = true;
-        ENV.APP.LOG_TRANSITIONS = true;
-        ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-        ENV.APP.LOG_VIEW_LOOKUPS = true;
+        ENV.APP.LOG_RESOLVER = false;
+        ENV.APP.LOG_ACTIVE_GENERATION = false;
+        ENV.APP.LOG_TRANSITIONS = false;
+        ENV.APP.LOG_TRANSITIONS_INTERNAL = false;
+        ENV.APP.LOG_VIEW_LOOKUPS = false;
         ENV.APP.GRANTS_BACKEND = 'http://127.0.0.1:8000/api';
     }
 //

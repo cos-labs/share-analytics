a5512d0c13e296066b3f91ca2b412f8e85641f72
Josh Bird <joshua.thomas.bird@gmail.com>
2017-03-08 13:01:14 -0500

    changeasset url

--- client/config/environment.js
+++ client/config/environment.js
@@ -73,7 +73,7 @@ module.exports = function(environment) {
 
     if (environment === 'production') {
         ENV.locationType = 'hash';
-        ENV.rootURL = '/share-analytics/client/';
+        ENV.rootURL = '/share-analytics/';
 
 
         ENV.baseURL = "share-analytics";

824aec38c31787f421816f4e11263a204b79a0cb
Josh Bird <joshua.thomas.bird@gmail.com>
2017-03-13 15:27:10 -0400

    styling for triton share title

--- config/environment.js
+++ config/environment.js
@@ -38,8 +38,8 @@ module.exports = function(environment) {
     };
     //this needs to go in an actual env at some point
     ENV.csrfCookie = 'csrftoken';
-    ENV.apiBaseUrl = 'https://staging-share.osf.io';
-    ENV.apiUrl = 'https://staging-share.osf.io/api/v2';
+    ENV.apiBaseUrl = 'https://share.osf.io';
+    ENV.apiUrl = 'https://share.osf.io/api/v2';
 
     if (environment === 'development') {
         ENV['ember-cli-mirage'] = {

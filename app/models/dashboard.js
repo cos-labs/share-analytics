064e2bcfcb2c95a085a2a88ff4212e1550eb9fe3
Saman Ehsan <se8ea@virginia.edu>
2017-04-17 15:44:14 -0400

    Update ember dashboard model

--- app/models/dashboard.js
+++ app/models/dashboard.js
@@ -3,10 +3,10 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 
     name: DS.attr('string'),
+    settings: DS.attr('object'),
     widgets: DS.hasMany('widget', {
         async: true,
         inverse: "dashboard"
-    }),
+    })
 
 });
-

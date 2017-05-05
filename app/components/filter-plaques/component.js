286a4352f85da13514b6fe822186faf5cd2b7c40
Saman Ehsan <se8ea@virginia.edu>
2017-05-05 16:17:52 -0400

    Replace tag ids with display name
    The ids for contributors, funders and publishers
    are used to filter the search results page. However,
    the tag that gets displayed should show the name of
    the contributor/funder/publisher instead of the id
    used for filtering.
    
    Make a request to the api gateway bulk_agents endpoint
    that makes a bulk request to SHARE to get record data.
    Then replace the tag ids with their correct display names.

--- app/components/filter-plaques/component.js
+++ app/components/filter-plaques/component.js
@@ -1,21 +1,52 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+var ID_FILTERS = ['contributors', 'funders', 'publishers'];
 
+export default Ember.Component.extend({
+    filters: null,
     init () {
-        this._super(...arguments);
-        let parameters = this.get('parameters');
-        this.set('filters', Object.keys(parameters).map((key, idx, arr) => {
-            return {
-                "key": key,
-                "value": parameters[key]
+      this._super(...arguments);
+      let parameters = this.get('parameters');
+      var filters = Object.keys(parameters).map((key) => {
+        return {
+          "key": key,
+          "value": parameters[key]
+        }
+      }).filter((item) => {
+        return item.key !== "page";
+      });
+
+      // Find and format all query params that use ids for the value
+      var ids = filters.filter((item) => {
+          return ID_FILTERS.indexOf(item.key) !== -1;
+      }).map((param) => {
+          return {key: param.value, doc_count: 0}
+      });
+
+      // Fetch display names
+      if (ids.length !== 0) {
+        this.fetchAgentDetails(ids).then((data) => {
+          for (var i = 0; i < data.length; i++) {
+            for (var filter = 0; filter < filters.length; filter++) {
+              if (filters[filter].value === data[i].id) {
+                  Ember.set(filters[filter], 'value', data[i].name);
+              }
             }
-        }).filter((item) => {
-            if (item.key == "page") {
-                return false;
-            }
-            return true;
-        }));
+          }
+        });
+      }
+      this.set('filters', filters);
+    },
+
+    fetchAgentDetails: async function(data) {
+        let agent_details = await Ember.$.ajax({
+            url: 'https://dev-labs.cos.io/bulk_get_agents',
+            crossDomain: true,
+            data: JSON.stringify(data),
+            type: 'POST',
+            contentType: 'application/json'
+        });
+        return JSON.parse(agent_details);
     },
 
     actions: {
